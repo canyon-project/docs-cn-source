2a0348973939635730f84e985fd89d48c5349751
Travis Zhang <wr.zhang25@gmail.com>
2026-01-22 11:16:17 +0800

    chore: daily development

---
+++ content/docs/core-concepts/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  'change-code-coverage': '变更代码覆盖率',
+  'accumulative-coverage': '跨 Commit 覆盖率累积',
+  'restore-source-code-coverage': '还原源代码覆盖率',
+  'separate-hit-and-map': '分离 Hit 和 Map',
+};
